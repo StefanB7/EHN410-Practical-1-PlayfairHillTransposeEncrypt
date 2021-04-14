044ec0ab59f70d08ce1bd349d47557cab98be096
Stefan Buys <22056509+StefanB7@users.noreply.github.com>
2021-04-14 22:05:30 +0200

    Playfair unit test klaar

--- stefan/test_playfaircipher.py
+++ stefan/test_playfaircipher.py
@@ -9,6 +9,8 @@ txtDEC = ""
 imgENC = None
 imgDEC = None
 
+##### TEXT DECRYPTION AND ENCRYPTION TESTING #####
+
 # Test plaintext that is too small 
 print("\n____________________________________________________")
 print("\nTest (1/7): Plaintext that is too small")
@@ -45,48 +47,50 @@ print("Encrypted text: ",txtENC)
 txtDEC = verander_die.Playfair_Decrypt("UgandaKenyaNigeria",txtENC)
 print("Decrypted text: ",txtDEC)
 
-# # Test a 2x2 key matrix
-# print("\n____________________________________________________")
-# print("\nTest (4/7): 2x2 key matrix")
-# print("\nInput:")
-# print("Plaintext: that's a fat cat")
-# print("Key: DDCF")
-# print("\nOutput:")
-# txtENC = verander_die.Hill_Encrypt("DDCF","that's a fat cat")
-# print("Encrypted text: ",txtENC)
-# txtDEC = verander_die.Hill_Decrypt("DDCF",txtENC)
-# print("Decrypted text: ",txtDEC)
-#
-# # Test a 3x3 key matrix
-# print("\n____________________________________________________")
-# print("\nTest (5/7): 3x3 key matrix")
-# print("\nInput:")
-# print("Plaintext: that's a fat cat")
-# print("Key: RRFVSVCCT")
-# print("\nOutput:")
-# txtENC = verander_die.Hill_Encrypt("RRFVSVCCT","that's a fat cat")
-# print("Encrypted text: ",txtENC)
-# txtDEC = verander_die.Hill_Decrypt("RRFVSVCCT",txtENC)
-# print("Decrypted text: ",txtDEC)
-#
-# # Test image that is not the correct length
-# print("\n____________________________________________________")
-# print("\nTest (6/7): Image that is not the correct size")
-# p_File = Image.open('EHN410_Prak1_PlayfairHillTransposeEncrypt\jacobus\images\office.png')
-# p_img = np.asarray(p_File)
-# imgENC = verander_die.Hill_Encrypt("RRFVSVCCT",p_img)
-# Image.fromarray(imgENC.astype(np.uint8)).save('EHN410_Prak1_PlayfairHillTransposeEncrypt\jacobus\images_test\office_enc_hill.png')
-# imgDEC = verander_die.Hill_Decrypt("RRFVSVCCT",imgENC)
-# Image.fromarray(imgDEC.astype(np.uint8)).save('EHN410_Prak1_PlayfairHillTransposeEncrypt\jacobus\images_test\office_dec_hill.png')
-#
-# # Test a when key matrix does not have a modular multiplicative inverse
-# print("\n____________________________________________________")
-# print("\nTest (7/7): When key matrix does not have a modular multiplicative inverse")
-# print("\nInput:")
-# print("Plaintext: that's a fat cat")
-# print("Key: Aristocat")
-# print("\nOutput:")
-# txtENC = verander_die.Hill_Encrypt("catamaran","that's a fat cat")
-# print("Encrypted text: ",txtENC)
-# txtDEC = verander_die.Hill_Decrypt("catamaran",txtENC)
-# print("Decrypted text: ",txtDEC)
+#Test a long key:
+print("\n____________________________________________________")
+print("\nTest (4/7): long key")
+print("\nInput:")
+print("Plaintext: that's a fat cat")
+print("Key: theenemyiswithinandwemig")
+print("\nOutput:")
+txtENC = verander_die.Playfair_Encrypt("theenemyiswithinandwemig","that's a fat cat")
+print("Encrypted text: ",txtENC)
+txtDEC = verander_die.Playfair_Decrypt("theenemyiswithinandwemig",txtENC)
+print("Decrypted text: ",txtDEC)
+
+# Test a long plaintext
+print("\n____________________________________________________")
+print("\nTest (5/7): long plaintext")
+print("\nInput:")
+print("Plaintext: Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. - Steve Jobs")
+print("Key: rickastley")
+print("\nOutput:")
+txtENC = verander_die.Playfair_Encrypt("rickastley","Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. - Steve Jobs")
+print("Encrypted text: ",txtENC)
+txtDEC = verander_die.Playfair_Decrypt("rickastley",txtENC)
+print("Decrypted text: ",txtDEC)
+
+##### IMAGE DECRYPTION AND ENCRYPTION TESTING #####
+
+#Test image encryption and decryption
+print("\n____________________________________________________")
+print("\nTest (6/7): Normal image encryption and decryption")
+print("Key: JacobusStefanEHNPrakties")
+p_File = Image.open('berge.png')
+p_img = np.asarray(p_File)
+imgENC = verander_die.Playfair_Encrypt("JacobusStefanEHNPrakties",p_img)
+Image.fromarray(imgENC.astype(np.uint8)).save('mountains_encrypted_playfair.png')
+imgDEC = verander_die.Playfair_Decrypt("JacobusStefanEHNPrakties",imgENC)
+Image.fromarray(imgDEC.astype(np.uint8)).save('mountains_decrypted_playfair.png')
+
+#Test another image encryption and decryption
+print("\n____________________________________________________")
+print("\nTest (7/7): Normal image encryption and decryption")
+print("Key: Therearenobearsinsouthaf")
+p_File = Image.open('beer.png')
+p_img = np.asarray(p_File)
+imgENC = verander_die.Playfair_Encrypt("Therearenobearsinsouthaf",p_img)
+Image.fromarray(imgENC.astype(np.uint8)).save('bear_encrypted_playfair.png')
+imgDEC = verander_die.Playfair_Decrypt("Therearenobearsinsouthaf",imgENC)
+Image.fromarray(imgDEC.astype(np.uint8)).save('bear_decrypted_playfair.png')
